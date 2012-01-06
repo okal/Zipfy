73fb7be2531bb6e5b3cd4a1c67c407325cfda58e
Okal Otieno <okal@justokal.com>
2012-01-06 12:30:14 +0300

    Corpus class functionally complete

--- parser.py
+++ parser.py
@@ -1,3 +1,5 @@
+import re
+
 from requests import get
 from BeautifulSoup import BeautifulSoup
 from HTMLParser import HTMLParser
@@ -9,3 +11,9 @@ class Corpus:
         self.body = self.page.body
         text = ' '.join(self.body.findAll(text=True)).strip()
         self.text = ' '.join(h.unescape(text).split()).lower()
+        self.words = re.sub('[^A-Za-z\'\s\-]+','', self.text)
+        self.word_list = self.words.split()
+        self.word_set = set(self.word_list)
+        self.freq_list = []
+        for word in self.word_set:
+            self.freq_list.append((word, self.word_list.count(word)))
