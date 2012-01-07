21efc1104f8688cff71ad953972deae0ee80d4a1
Okal Otieno <okal@justokal.com>
2012-01-07 14:08:14 +0300

    Added redis support to cache repeated requests

--- app.py
+++ app.py
@@ -1,22 +1,48 @@
+import redis
+
+from hashlib import sha1
 from flask import Flask, request, abort, render_template, make_response
 from zipfy import Corpus
 from json.encoder import JSONEncoder
 
 app = Flask(__name__)
+#app.debug = True
+try:
+    from bundle_config import config
+    r = redis.Redis(
+        host = config['redis']['host'],
+        port = config['redis']['port'],
+        password = config['redis']['password']
+    )
+except:
+    r = redis.StrictRedis(host='localhost', port=6379, db=0)
 
 @app.route('/', methods=['GET', 'POST'])
-def hello_world():
+def index():
     if request.method == 'POST':
-        try:
-            corpus = Corpus(request.form['url'])
-        except:
-            return "Hello, Post!" 
-
+        site_hash = sha1(request.form['url']).hexdigest()
+        zipf_profile = r.get(site_hash)
+        if zipf_profile: 
+            return zipf_profile
+        else:
+            try:
+                corpus = Corpus(request.form['url'])
+            except:
+                return "Hello, Post!" 
+        
         e = JSONEncoder()
-        resp = make_response(e.encode(corpus.freq_list))
+        zipf_profile = e.encode(corpus.freq_list)
+        r.set(site_hash, zipf_profile)
+        resp = make_response(zipf_profile)
         return resp
     elif request.method == 'GET':
         return render_template("index.html")
 
+#@app.route('/<site_hash>/')
+#def from_slug(site_hash):
+#    zipf_profile = r.get(site_hash)
+#    if zipf_profile:
+#        return render_template("index.html", zipf_profile=zipf_profile)
+    
 if __name__ == "__main__":
     app.run()
